7e570ec59df30570171484cb855fbd6c71f6a01f
Andre Markov <andremarkov@icloud.com>
2024-11-19 14:55:44 +0300

    add nuxt and nuxt-buefy

---
+++ my-custom-dir/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Program%20Files/OSPanel/domains/qr-app/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
+function defineNuxtConfig(param: {compatibilityDate: string; vite: {server: {proxy: {"/api": string}}}; buildDir: string; devtools: {enabled: boolean}}) {
+
+}
+
 export default defineNuxtConfig({
+    buildDir: '.output',
     devtools: { enabled: true },
     compatibilityDate: "2024-11-19",
     vite: {
