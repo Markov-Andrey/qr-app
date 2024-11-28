a54e2f6fb2d9be06ee9af350715c192bfa01f307
Andre Markov <andremarkov@icloud.com>
2024-11-28 17:20:34 +0300

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>QR-Генератор</title>
+    <title>QR-Gen</title>
     @vite('resources/css/app.css')
 </head>
 <body>
