f00a506c8b5966c098fb19e0e645a8efa08d9b8b
Andre Markov <andremarkov@icloud.com>
2024-11-29 16:15:49 +0300

    add view

--- resources/js/components/PreviewComponent.vue
+++ resources/js/components/PreviewComponent.vue
@@ -2,7 +2,7 @@
     <div class="bg-gray-100 p-4 rounded-md max-h-96 overflow-y-auto">
         <pre>{{ content || 'Нет данных для отображения' }}</pre>
     </div>
-    <div class="m-4 flex gap-2">
+    <div class="my-4 flex justify-between gap-2">
         <v-btn prepend-icon="mdi-chevron-left-circle" color="orange">Назад</v-btn>
         <v-btn append-icon="mdi-chevron-right-circle" color="primary">Далее</v-btn>
     </div>
