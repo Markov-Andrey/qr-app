38c07747d9615227f9af34d5d09c8496186798d6
Andre Markov <andremarkov@icloud.com>
2024-12-02 15:58:05 +0300

    stepper next

--- resources/js/components/QRCodeRenderer.vue
+++ resources/js/components/QRCodeRenderer.vue
@@ -1,17 +1,55 @@
 <template>
-    <div ref="printContent" class="flex flex-wrap gap-4 relative min-h-36">
-        <div
-            v-if="loading"
-            class="absolute inset-0 flex items-center justify-center bg-white/50 backdrop-blur-lg z-10"
-        >
-            <v-progress-circular
-                indeterminate
-                size="64"
-                color="primary"
-            />
+    <div>
+        <div class="flex mb-4">
+            <v-menu>
+                <template v-slot:activator="{ props }">
+                    <v-btn class="w-[175px]" v-bind="props" color="primary">
+                        Размер {{ selectedSize.text }}
+                        <v-icon class="mr-1">mdi-menu-down</v-icon>
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item
+                        v-for="(item, index) in items"
+                        :key="index"
+                        @click="selectSize(item)"
+                        :class="{'bg-primary text-white': selectedSize.text === item.text}"
+                    >
+                        <v-list-item-title>{{ item.text }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+
+            <v-divider class="border-opacity-100 mx-3" vertical />
+
+            <v-btn @click="printQRCode" color="primary">
+                <v-icon class="mr-2">mdi-printer</v-icon>
+                Печать
+            </v-btn>
+
+            <v-divider class="border-opacity-100 mx-3" vertical />
+
+            <v-btn color="primary">
+                <v-icon class="mr-2">mdi-download</v-icon>
+                Скачать
+            </v-btn>
         </div>
-        <div class="flex" v-for="(qr, index) in qrCodes" :key="index">
-            <div v-if="qr" v-html="qr" />
+    </div>
+    <div id="qr-renderer">
+        <div class="flex flex-wrap gap-4 relative min-h-36">
+            <div
+                v-if="loading"
+                class="absolute inset-0 flex items-center justify-center bg-white/50 backdrop-blur-lg z-10"
+            >
+                <v-progress-circular
+                    indeterminate
+                    size="64"
+                    color="primary"
+                />
+            </div>
+            <div v-for="(qr, index) in qrCodes" :key="index">
+                <div v-if="qr" v-html="qr" />
+            </div>
         </div>
     </div>
 </template>
@@ -29,7 +67,18 @@ const props = defineProps({
 
 const qrCodes = ref([]);
 const loading = ref(false);
+const items = ref([
+    { text: '10x10', value: [1, 1] },
+    { text: '20x20', value: [2, 2] },
+    { text: '30x30', value: [3, 3] },
+    { text: '40x40', value: [4, 4] },
+    { text: '50x50', value: [5, 5] },
+]);
+const selectedSize = ref(items.value[0]);
 
+function selectSize(item) {
+    selectedSize.value = item;
+}
 async function generateQRCodes(fileContent) {
     loading.value = true;
     try {
@@ -37,15 +86,50 @@ async function generateQRCodes(fileContent) {
         const response = await apiService.uploadFile({ codes });
 
         if (response && response.data) {
-            qrCodes.value = response.data.map(qrCode => {
-                return `<div class="qr-code">${qrCode}</div>`;
-            });
+            qrCodes.value = response.data;
         }
     } catch (error) {
         console.error("Ошибка при генерации QR-кодов:", error);
     } finally {
         loading.value = false;
     }
+}
+function printQRCode() {
+    const qrRenderer = document.querySelector('#qr-renderer');
+    const printWindow = window.open('', '_blank');
+
+    const styles = Array.from(document.styleSheets)
+        .map((sheet) => {
+            try {
+                return Array.from(sheet.cssRules)
+                    .map((rule) => rule.cssText)
+                    .join('');
+            } catch (e) {
+                console.warn('Cannot access stylesheet:', sheet, e);
+                return '';
+            }
+        })
+        .join('');
+
+    printWindow.document.write(`
+        <html>
+        <head>
+            <title>Печать QR-кодов</title>
+            <style>${styles}</style>
+        </head>
+        <body>
+            ${qrRenderer.innerHTML}
+        </body>
+        </html>
+    `);
+
+    printWindow.focus();
+    printWindow.print();
+    printWindow.close();
+
+    printWindow.onafterprint = () => {
+        printWindow.close();
+    };
 }
 watch(() => props.file, (newFile) => {
     if (newFile) {
