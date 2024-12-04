a1567a27fa2b48997d6d09c38a107cde1657b804
Andre Markov <andremarkov@icloud.com>
2024-12-04 15:32:33 +0300

    add user resource

---
+++ app/MoonShine/Resources/UserResource.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+use MoonShine\Fields\Date;
+use MoonShine\Fields\Text;
+use MoonShine\Resources\ModelResource;
+use MoonShine\Decorations\Block;
+use MoonShine\Fields\ID;
+use MoonShine\Fields\Field;
+use MoonShine\Components\MoonShineComponent;
+
+/**
+ * @extends ModelResource<User>
+ */
+class UserResource extends ModelResource
+{
+    protected string $model = User::class;
+
+    protected string $title = 'Пользователи';
+
+    /**
+     * @return list<MoonShineComponent|Field>
+     */
+    public function fields(): array
+    {
+        return [
+            Block::make([
+                ID::make()->sortable(),
+                ID::make()->sortable(),
+                Text::make('Имя пользователя', 'name'),
+                Text::make('Email', 'email'),
+                Date::make('Email проверен', 'email_verified_at')->format('d.m.Y'),
+                Date::make('Создано', 'created_at')->format('d.m.Y'),
+                Date::make('Обновлено', 'updated_at')->format('d.m.Y'),
+            ]),
+        ];
+    }
+
+    /**
+     * @param User $item
+     *
+     * @return array<string, string[]|string>
+     * @see https://laravel.com/docs/validation#available-validation-rules
+     */
+    public function rules(Model $item): array
+    {
+        return [];
+    }
+}
