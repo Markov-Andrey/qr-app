fad41520feae82fb434ef84d1241561300b659d5
Andre Markov <andremarkov@icloud.com>
2025-03-04 11:27:57 +0300

    upd

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
 import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
         vue(),
     ],
     build: {
         outDir: 'public/build',
         rollupOptions: {
+            input: 'resources/js/app.js',
             output: {
                 entryFileNames: 'app.js',
+                chunkFileNames: 'chunks/[name]-[hash].js',
             },
-            input: 'resources/js/app.js',
         },
     },
     resolve: {
