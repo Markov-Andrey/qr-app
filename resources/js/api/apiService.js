2d3229c9ea37ecea53e0853661356a5ddd0d9930
Andre Markov <andremarkov@icloud.com>
2025-03-11 09:35:51 +0300

    upd cache

--- resources/js/api/apiService.js
+++ resources/js/api/apiService.js
@@ -8,19 +8,19 @@ class ApiService {
         return request(() => api().post(`api/login`, data));
     }
     async register(data) {
-        return request(() => api().post('api/signup/', data));
+        return request(() => api().post('api/signup', data));
     }
     async history() {
-        return request(() => api(this.withToken).get('api/history/'));
+        return request(() => api(this.withToken).get('api/history'));
     }
     async uploadFile(data) {
-        return request(() => api(this.withToken).post('api/get-svg/', { codes: data.codes }));
+        return request(() => api(this.withToken).post('api/get-svg', { codes: data.codes }));
     }
     async resetPassword(data) {
-        return request(() => api().post('api/reset-password/', data));
+        return request(() => api().post('api/reset-password', data));
     }
     async updatePassword(data) {
-        return request(() => api().post('api/update-password/', data));
+        return request(() => api().post('api/update-password', data));
     }
 }
 export const apiService = new ApiService();
